2d161a7e1f97e5a6500a63a6b443628b7cd4088f
Pawan Shah <pa1.shah411@gmail.com>
2022-07-08 15:58:48 +0530

    Changes in AddUsers and new Java class Pair.java added

--- app/src/main/java/com/example/tripisss/AddUsers.java
+++ app/src/main/java/com/example/tripisss/AddUsers.java
@@ -1,37 +1,87 @@
 package com.example.tripisss;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class AddUsers extends AppCompatActivity {
-ArrayList<String> list;
-String[] names;
+    ArrayList<String> list;
+    ArrayList<String> names;
+    LinearLayout ly;
+    String city;
+    String user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_users);
         TextView trip = findViewById(R.id.trip);
         Intent intent = getIntent();
-        String city = intent.getStringExtra("tn");
+        city = intent.getStringExtra("tn");
         city = "Trip to "+ city;
+        user = FirebaseAuth.getInstance().getCurrentUser().getEmail().toString();
+        Toast.makeText(getApplicationContext(), user, Toast.LENGTH_SHORT).show();
         trip.setText(city);
         list = new ArrayList<String>();
-        String[] name1 = {"Pawan","array","Bishesh","Mehul","Pawan","array","Bishesh","Mehul","Pawan","array","Bishesh","Mehul"};
-        names = name1;
-        LinearLayout ly = findViewById(R.id.linear);
-        for(int i=0; i< names.length; i++){
-            ly.addView(create(names[i],i));
-            ly.addView(space());
-        }
+        names = new ArrayList<String>();
+        ly = findViewById(R.id.linear);
+        FirebaseDatabase.getInstance().getReference().child("Users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot s : snapshot.getChildren()){
+                    String kl = s.getValue().toString();
+                    if(names.contains(kl)==false)
+                    {
+                        names.add(s.getValue().toString());
+                        ly.addView(create(kl, names.size()-1));
+                        ly.addView(space());
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        Button pro = findViewById(R.id.proceedBtn);
+        pro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseDatabase.getInstance().getReference()
+                        .child(city).child("max").setValue(new Pair(user, 0));
+
+                FirebaseDatabase.getInstance().getReference()
+                        .child(city).child("min").setValue(new Pair(user, 0));
+
+                FirebaseDatabase.getInstance().getReference()
+                        .child(city).child("total").setValue(0);
+
+                FirebaseDatabase.getInstance().getReference()
+                        .child(city).child("credit").setValue(0);
+
+                Intent it = new Intent(getApplicationContext(),Expense.class);
+                startActivity(it);
+            }
+        });
     }
 
     public LinearLayout space() {
@@ -47,7 +97,7 @@ String[] names;
         lly.setBackgroundColor(getResources().getColor(R.color.white));
         TextView txt = new TextView(getApplicationContext());
         txt.setLayoutParams(new LinearLayout.LayoutParams(750,150));
-        txt.setTextSize(30);
+        txt.setTextSize(20);
         name = "   "+name;
         txt.setText(name);
         ImageView iv = new ImageView(getApplicationContext());
@@ -62,7 +112,7 @@ String[] names;
             public void onClick(View v) {
                 iv1.setImageResource(R.drawable.ok);
                 int kl = iv1.getId();
-                list.add(names[kl-100]);
+                list.add(names.get(kl-100));
                 int idx = list.size()-1;
                 Toast.makeText(getApplicationContext(),list.get(idx),Toast.LENGTH_LONG).show();
             }
---
+++ app/src/main/java/com/example/tripisss/Pair.java
@@ -0,0 +1,11 @@
+package com.example.tripisss;
+
+public class Pair
+{
+   public String name;
+   public int value;
+    Pair(String name,int value){
+        this.name = name;
+        this.value = value;
+    }
+}
